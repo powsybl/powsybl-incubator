2dcd7389f81265e6df0a10dbf5e382d00f66facd
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2019-04-17 13:23:57 +0200

    Add substation diagram generation tool (#63)
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- substation-diagram/substation-diagram-util/src/main/java/com/powsybl/substationdiagram/util/NominalVoltageSubstationDiagramStyleProvider.java
+++ substation-diagram/substation-diagram-util/src/main/java/com/powsybl/substationdiagram/util/NominalVoltageSubstationDiagramStyleProvider.java
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package com.powsybl.substationdiagram.view.app;
+package com.powsybl.substationdiagram.util;
 
 import com.powsybl.iidm.network.VoltageLevel;
 import com.powsybl.substationdiagram.model.Edge;

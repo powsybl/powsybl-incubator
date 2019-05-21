0c7a8873479715578861f19f6fe50e696d953f05
Marcos de Miguel <demiguelm@aia.es>
2019-05-21 12:36:21 +0200

    unitary tests
    Signed-off-by: Marcos de Miguel <demiguelm@aia.es>

--- cgmes-conversion-validation/src/test/java/com/powsybl/cgmes/conversion/test/CgmesValidationTest.java
+++ cgmes-conversion-validation/src/test/java/com/powsybl/cgmes/conversion/test/CgmesValidationTest.java
@@ -7,20 +7,35 @@
 
 package com.powsybl.cgmes.conversion.test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.google.common.io.ByteStreams;
 import com.google.common.jimfs.Configuration;
 import com.google.common.jimfs.Jimfs;
 import com.powsybl.cgmes.conformity.test.CgmesConformity1Catalog;
+import com.powsybl.cgmes.conversion.validation.TerminalFlow;
+import com.powsybl.cgmes.conversion.validation.TerminalFlow.BranchEndType;
+import com.powsybl.cgmes.conversion.validation.TerminalFlow.CgmesFlow;
 import com.powsybl.cgmes.conversion.validation.ValidationResults;
+import com.powsybl.cgmes.conversion.validation.ValidationResults.ValidationAlternativeResults;
 import com.powsybl.cgmes.conversion.validation.model.CgmesModelConversion;
 import com.powsybl.cgmes.conversion.validation.model.CgmesModelConversionFactory;
+import com.powsybl.cgmes.conversion.validation.report.CatalogValidationReport;
+import com.powsybl.cgmes.interpretation.model.interpreted.InterpretationAlternative;
 import com.powsybl.cgmes.model.test.TestGridModel;
 import com.powsybl.commons.config.InMemoryPlatformConfig;
 import com.powsybl.commons.datasource.ReadOnlyDataSource;
@@ -43,45 +58,71 @@ public class CgmesValidationTest {
 
     @Test
     public void microGridBaseCaseBE() throws IOException {
-        test(catalog.microGridBaseCaseBE(), "microGridBaseCaseBE");
+        ValidationResults conversionValidationResult = test(catalog.microGridBaseCaseBE(), "microGridBaseCaseBE");
+        assertFalse(getFailed(conversionValidationResult));
     }
 
     @Test
     public void microGridBaseCaseNL() throws IOException {
-        test(catalog.microGridBaseCaseNL(), "microGridBaseCaseNL");
+        ValidationResults conversionValidationResult = test(catalog.microGridBaseCaseNL(), "microGridBaseCaseNL");
+        assertFalse(getFailed(conversionValidationResult));
     }
 
     @Test
     public void microGridBaseCaseAssembled() throws IOException {
-        test(catalog.microGridBaseCaseAssembled(), "microGridBaseCaseAssembled");
+        ValidationResults conversionValidationResult = test(catalog.microGridBaseCaseAssembled(), "microGridBaseCaseAssembled");
+        assertFalse(getFailed(conversionValidationResult));
     }
 
     @Test
     public void microGridType4BE() throws IOException {
-        test(catalog.microGridType4BE(), "microGridType4BE");
+        ValidationResults conversionValidationResult = test(catalog.microGridType4BE(), "microGridType4BE");
+        assertFalse(getFailed(conversionValidationResult));
     }
 
     @Test
     public void miniBusBranch() throws IOException {
-        test(catalog.miniBusBranch(), "miniBusBranch");
+        ValidationResults conversionValidationResult = test(catalog.miniBusBranch(), "miniBusBranch");
+        assertFalse(getFailed(conversionValidationResult));
     }
 
     @Test
     public void miniNodeBreaker() throws IOException {
-        test(catalog.miniNodeBreaker(), "miniNodeBreaker");
+        ValidationResults conversionValidationResult = test(catalog.miniNodeBreaker(), "miniNodeBreaker");
+        assertFalse(getFailed(conversionValidationResult));
     }
 
     @Test
     public void smallBusBranch() throws IOException {
-        test(catalog.smallBusBranch(), "smallBusBranch");
+        ValidationResults conversionValidationResult = test(catalog.smallBusBranch(), "smallBusBranch");
+        assertFalse(getFailed(conversionValidationResult));
     }
 
     @Test
     public void smallNodeBreaker() throws IOException {
-        test(catalog.smallNodeBreaker(), "smallNodeBreaker");
+        ValidationResults conversionValidationResult = test(catalog.smallNodeBreaker(), "smallNodeBreaker");
+        assertFalse(getFailed(conversionValidationResult));
     }
 
-    private void test(TestGridModel testGridModel, String modelName) throws IOException {
+    @Test
+    public void report() throws IOException {
+        Collection<ValidationResults> results = new ArrayList<>();
+        ValidationResults r = new ValidationResults("test");
+        InterpretationAlternative alternative = new InterpretationAlternative();
+        ValidationAlternativeResults validation = new ValidationAlternativeResults(alternative);
+        CgmesFlow cgmesFlow = new CgmesFlow(-10.0, -10.0, true);
+        TerminalFlow terminalFlow = new TerminalFlow("test", BranchEndType.LINE_ONE, 10.0, 10.0, cgmesFlow);
+        validation.addTerminalFlow(terminalFlow);
+        r.validationAlternativeResults().put(alternative, validation);
+        results.add(r);
+        try (FileSystem fs = Jimfs.newFileSystem(Configuration.unix())) {
+            Files.createDirectory(fs.getPath("tmp"));
+            new CatalogValidationReport(fs.getPath("tmp")).report(results);
+            compareTxt(getClass().getResourceAsStream("/CGMESCatalogValidation"), Files.newInputStream(fs.getPath("tmp", "CGMESCatalogValidation")));
+        }
+    }
+
+    private ValidationResults test(TestGridModel testGridModel, String modelName) throws IOException {
         ReadOnlyDataSource ds = testGridModel.dataSource();
         String impl = TripleStoreFactory.defaultImplementation();
         double threshold = 0.01;
@@ -90,8 +131,7 @@ public class CgmesValidationTest {
             ValidationConfig config = loadFlowParametersConfig(fs, threshold);
             CgmesModelConversion model = (CgmesModelConversion) CgmesModelConversionFactory.create(modelName, ds, impl);
             model.z0Nodes();
-            ValidationResults conversionValidationResult = new CgmesValidationTester(model).test(config);
-            assertFalse(getFailed(conversionValidationResult));
+            return new CgmesValidationTester(model).test(config);
         }
     }
 
@@ -110,6 +150,29 @@ public class CgmesValidationTest {
         config.setOkMissingValues(false);
         config.setLoadFlowParameters(new LoadFlowParameters());
         return config;
+    }
+
+    private static String normalizeLineSeparator(String str) {
+        return str.replace("\r\n", "\n")
+                .replace("\r", "\n");
+    }
+
+    private static void compareTxt(InputStream expected, InputStream actual) {
+        try {
+            compareTxt(expected, new String(ByteStreams.toByteArray(actual), StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private static void compareTxt(InputStream expected, String actual) {
+        try {
+            String expectedStr = normalizeLineSeparator(new String(ByteStreams.toByteArray(expected), StandardCharsets.UTF_8));
+            String actualStr = normalizeLineSeparator(actual);
+            assertEquals(expectedStr, actualStr);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 
     private static CgmesConformity1Catalog catalog;

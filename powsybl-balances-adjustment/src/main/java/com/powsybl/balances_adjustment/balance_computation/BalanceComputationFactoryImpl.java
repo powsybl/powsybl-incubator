cea561f6347409884ebc0b289ec58510415f728b
WalAmeni <42716685+WalAmeni@users.noreply.github.com>
2019-05-22 17:09:08 +0200

    Balances adjustment (#67)
    * Adding balances adjustment feature
    * Testing with Mockito
    
    Signed-off-by: Ameni Walha <ameni.walha@rte-france.com>

---
+++ powsybl-balances-adjustment/src/main/java/com/powsybl/balances_adjustment/balance_computation/BalanceComputationFactoryImpl.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2019, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.balances_adjustment.balance_computation;
+
+import com.powsybl.action.util.Scalable;
+import com.powsybl.balances_adjustment.util.NetworkArea;
+import com.powsybl.computation.ComputationManager;
+import com.powsybl.iidm.network.Network;
+import com.powsybl.loadflow.LoadFlowFactory;
+
+import java.util.Map;
+
+/**
+ * Balance computation factory to create <code>BalanceComputationImpl</code> class
+ * @author Ameni Walha <ameni.walha at rte-france.com>
+ */
+public class BalanceComputationFactoryImpl implements BalanceComputationFactory {
+
+    @Override
+    public BalanceComputation create(Network network, Map<NetworkArea, Double> networkAreaNetPositionTargetMap, Map<NetworkArea, Scalable> networkAreasScalable, LoadFlowFactory loadFlowFactory, ComputationManager computationManager, int priority) {
+        return new BalanceComputationImpl(network, networkAreaNetPositionTargetMap, networkAreasScalable, computationManager, loadFlowFactory);
+    }
+}

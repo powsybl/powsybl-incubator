20fb9ebb284f8b868cbcfc7c8c6dd744d899f4e9
massimo.ferraro <massimo.ferraro@techrain.eu>
2019-06-18 15:33:00 +0200

    Add IIDM XML converters for GL extensions, see issue #90
    Signed-off-by: massimo.ferraro <massimo.ferraro@techrain.eu>

--- cgmes-gl/cgmes-gl-iidm-extensions/src/test/java/com/powsybl/cgmes/iidm/extensions/gl/PositionTest.java
+++ cgmes-gl/cgmes-gl-iidm-extensions/src/test/java/com/powsybl/cgmes/iidm/extensions/gl/PositionTest.java
@@ -6,22 +6,30 @@
  */
 package com.powsybl.cgmes.iidm.extensions.gl;
 
+import static com.powsybl.cgmes.iidm.extensions.gl.GLTestUtils.LINE_1;
+import static com.powsybl.cgmes.iidm.extensions.gl.GLTestUtils.LINE_2;
+import static com.powsybl.cgmes.iidm.extensions.gl.GLTestUtils.SUBSTATION_1;
+import static com.powsybl.cgmes.iidm.extensions.gl.GLTestUtils.SUBSTATION_2;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
 import com.google.common.collect.ImmutableList;
+import com.powsybl.commons.AbstractConverterTest;
 import com.powsybl.iidm.network.Line;
 import com.powsybl.iidm.network.Network;
 import com.powsybl.iidm.network.Substation;
-import org.junit.Test;
-
-import static com.powsybl.cgmes.iidm.extensions.gl.GLTestUtils.*;
+import com.powsybl.iidm.xml.NetworkXml;
 
 /**
  *
  * @author Massimo Ferraro <massimo.ferraro@techrain.eu>
  */
-public class PositionTest {
+public class PositionTest extends AbstractConverterTest {
 
     @Test
-    public void test() {
+    public void test() throws IOException {
         Network network = GLTestUtils.getNetwork();
         Substation substation1 = network.getSubstation("Substation1");
         SubstationPosition substationPosition1 = new SubstationPosition(substation1, SUBSTATION_1);
@@ -34,7 +42,12 @@ public class PositionTest {
         Line line = network.getLine("Line");
         line.addExtension(LinePosition.class, new LinePosition<>(line, ImmutableList.of(SUBSTATION_1, LINE_1, LINE_2, SUBSTATION_2)));
 
-        GLTestUtils.checkNetwork(network);
+        Network network2 = roundTripXmlTest(network,
+                                            NetworkXml::writeAndValidate,
+                                            NetworkXml::read,
+                                            "/network.xml");
+
+        GLTestUtils.checkNetwork(network2);
     }
 
 }

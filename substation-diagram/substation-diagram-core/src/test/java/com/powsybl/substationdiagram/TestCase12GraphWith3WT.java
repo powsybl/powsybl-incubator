be6470a9bd46fb19149fc1067e060d887be3cf99
Franck LECUYER <franck.lecuyer@rte-france.com>
2019-07-30 14:00:35 +0200

    Correcting checkstyle problems
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- substation-diagram/substation-diagram-core/src/test/java/com/powsybl/substationdiagram/TestCase12GraphWith3WT.java
+++ substation-diagram/substation-diagram-core/src/test/java/com/powsybl/substationdiagram/TestCase12GraphWith3WT.java
@@ -40,7 +40,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 /**

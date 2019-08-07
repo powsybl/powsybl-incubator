df9623fa32ed508dd8c5c4e48e792431c8674266
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2019-08-07 23:29:36 +0200

    Fix cyclic dependencies
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>

--- simple-loadflow/src/main/java/com/powsybl/loadflow/simple/equations/VoltageInitializer.java
+++ simple-loadflow/src/main/java/com/powsybl/loadflow/simple/equations/VoltageInitializer.java
@@ -6,8 +6,6 @@
  */
 package com.powsybl.loadflow.simple.equations;
 
-import com.powsybl.loadflow.LoadFlowParameters;
-import com.powsybl.loadflow.simple.ac.nr.DcValueVoltageInitializer;
 import com.powsybl.loadflow.simple.network.LfBus;
 import com.powsybl.loadflow.simple.network.LfNetwork;
 import com.powsybl.math.matrix.MatrixFactory;
@@ -16,19 +14,6 @@ import com.powsybl.math.matrix.MatrixFactory;
  * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
  */
 public interface VoltageInitializer {
-
-    static VoltageInitializer getFromParameters(LoadFlowParameters parameters) {
-        switch (parameters.getVoltageInitMode()) {
-            case UNIFORM_VALUES:
-                return new UniformValueVoltageInitializer();
-            case PREVIOUS_VALUES:
-                return new PreviousValueVoltageInitializer();
-            case DC_VALUES:
-                return new DcValueVoltageInitializer();
-            default:
-                throw new UnsupportedOperationException("Unsupported voltage init mode: " + parameters.getVoltageInitMode());
-        }
-    }
 
     void prepare(LfNetwork network, MatrixFactory matrixFactory);
 

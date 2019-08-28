ec9e27e06b938fa2bb8fa88df2455b5cc5e54c78
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2019-08-28 11:28:07 +0200

    Fix line separator
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- substation-diagram/substation-diagram-core/src/test/java/com/powsybl/substationdiagram/AbstractTestCase.java
+++ substation-diagram/substation-diagram-core/src/test/java/com/powsybl/substationdiagram/AbstractTestCase.java
@@ -45,8 +45,8 @@ public abstract class AbstractTestCase {
     protected final SubstationDiagramStyleProvider styleProvider = new DefaultSubstationDiagramStyleProvider();
 
     protected static String normalizeLineSeparator(String str) {
-        return str.replace("\r", "");
-//                .replace("\r", "\n");
+        return str.replace("\r\n", "\n")
+                .replace("\r", "\n");
     }
 
     abstract void setUp() throws IOException;

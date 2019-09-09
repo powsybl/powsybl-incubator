47538dfedd0df06a7a5b968a44fa7dd196ab961a
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2019-09-09 15:20:51 +0200

    Fix copyright
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- substation-diagram/substation-diagram-core/src/test/java/com/powsybl/substationdiagram/svg/TestRGBColor.java
+++ substation-diagram/substation-diagram-core/src/test/java/com/powsybl/substationdiagram/svg/TestRGBColor.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2019, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.substationdiagram.svg;
 
 import static org.junit.Assert.assertEquals;
@@ -8,6 +14,9 @@ import org.junit.Test;
 
 import com.powsybl.substationdiagram.util.RGBColor;
 
+/**
+ * @author Giovanni Ferrari <giovanni.ferrari at techrain.eu>
+ */
 public class TestRGBColor {
 
     @Test

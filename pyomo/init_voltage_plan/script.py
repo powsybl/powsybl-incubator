f01a657442b59bf8dcd29b5c3813eeb75aaff021
Hadrien <hadrien.godard@artelys.com>
2022-03-04 14:02:40 +0100

    Improve using iterrows
    Signed-off-by: Hadrien <hadrien.godard@artelys.com>

--- pyomo/init_voltage_plan/script.py
+++ pyomo/init_voltage_plan/script.py
@@ -31,12 +31,14 @@ slack_bus = ""
 voltage_level_slack = 0
 neighbours_slack = 0
 PV_neighbours = nb_neighboursPV(n, PV_buses)
-for i in PV_buses:
-    if n.get_voltage_levels().at[n.get_buses().at[i, "voltage_level_id"], "nominal_v"] >= voltage_level_slack:
-        if PV_neighbours[i] > neighbours_slack:
-            slack_bus = i
-            voltage_level_slack = n.get_voltage_levels().at[n.get_buses().at[i, "voltage_level_id"], "nominal_v"]
-            neighbours_slack = PV_neighbours[i]
+for id_b, row in n.get_buses().iterrows():
+    if id_b not in PV_buses:
+        continue
+    if n.get_voltage_levels().at[row["voltage_level_id"], "nominal_v"] >= voltage_level_slack:
+        if PV_neighbours[id_b] > neighbours_slack:
+            slack_bus = id_b
+            voltage_level_slack = n.get_voltage_levels().at[row["voltage_level_id"], "nominal_v"]
+            neighbours_slack = PV_neighbours[id_b]
 
 # Create variables
 model.V = pyo.Var(buses, domain=pyo.NonNegativeReals) # TO DO : add default values
@@ -46,21 +48,18 @@ model.Q = pyo.Var(PV_buses) # TO DO : add default values
 # Objectif function
 def obj_expression(m):
     res = 0
-    for bus in PQ_buses:
-        if not np.isnan(n.get_buses().at[bus, "v_mag"]):
-            v_pu_coeff = n.get_voltage_levels().at[n.get_buses().at[bus, "voltage_level_id"], "nominal_v"]
-            bus_v_pu = n.get_buses().at[bus, "v_mag"] / v_pu_coeff # TO DO: to be replaced with pu getters
+    for id_b, row in n.get_buses().iterrows():
+        if id_b not in PQ_buses:
+            continue
+        if not np.isnan(row["v_mag"]):
+            v_pu_coeff = n.get_voltage_levels().at[row["voltage_level_id"], "nominal_v"]
+            bus_v_pu = row["v_mag"] / v_pu_coeff # TO DO: to be replaced with pu getters
             res += (m.V[bus] - bus_v_pu) * (m.V[bus] - bus_v_pu)
     return res
 
 model.OBJ = pyo.Objective(rule=obj_expression)
 
 # Constraints
-lines = n.get_lines().index.tolist()
-two_w_transf = n.get_2_windings_transformers().index.tolist()
-generators = n.get_generators().index.tolist()
-loads = n.get_loads().index.tolist()
-
 ## Active power balance
 buses_wo_slack = []
 for bus in buses:
@@ -68,67 +67,72 @@ for bus in buses:
         continue
     buses_wo_slack.append(bus)
 
-model.I = pyo.Set(initialize=[slack_bus])
+model.I = pyo.Set(initialize=buses_wo_slack)
 
 def active_power_balance_expr(m,i):
     v_pu_coeff_i = n.get_voltage_levels().at[n.get_buses().at[i, "voltage_level_id"], "nominal_v"]
     lhs = 0
     rhs = 0
-    for line in lines:
-        if n.get_lines().at[line, "bus1_id"] == i and n.get_lines().at[line, "bus2_id"] in buses:
-            j = n.get_lines().at[line, "bus2_id"]
+    is_empty = True
+    for id_line, row in n.get_lines().iterrows():
+        if row["bus1_id"] == i and row["bus2_id"] in buses:
+            is_empy = False
+            j = row["bus2_id"]
             v_pu_coeff_j = n.get_voltage_levels().at[n.get_buses().at[j, "voltage_level_id"], "nominal_v"]
             sus_pu_coeff = p_pu / v_pu_coeff_i / v_pu_coeff_j #POWER/U/U 
             ratio_tr_pu = 1
-            line_g1_pu = n.get_lines().at[line, "g1"] / sus_pu_coeff # TO DO: to be replaced with pu getters
-            line_r = n.get_lines().at[line, "r"]
-            line_x = n.get_lines().at[line, "x"]
+            line_g1_pu = row[ "g1"] / sus_pu_coeff # TO DO: to be replaced with pu getters
+            line_r = row["r"]
+            line_x = row["x"]
             line_y_pu = 1 / np.sqrt(line_r * line_r + line_x * line_x) / sus_pu_coeff
             line_ksi = np.arctan2(line_r,line_x)
             lhs += ratio_tr_pu * m.V[i] * ( line_g1_pu * ratio_tr_pu * m.V[i] + line_y_pu * ratio_tr_pu * m.V[i] * pyo.sin(line_ksi) - line_y_pu * 1 * m.V[j] * pyo.sin(line_ksi - 0 + 0 - m.Phi[i] + m.Phi[j]) )
-        elif n.get_lines().at[line, "bus2_id"] == i and n.get_lines().at[line, "bus1_id"] in buses:
-            j = n.get_lines().at[line, "bus1_id"]
+        elif row["bus2_id"] == i and row["bus1_id"] in buses:
+            is_empy = False
+            j = row["bus1_id"]
             v_pu_coeff_j = n.get_voltage_levels().at[n.get_buses().at[j, "voltage_level_id"], "nominal_v"]
             sus_pu_coeff = p_pu / v_pu_coeff_i / v_pu_coeff_j #POWER/U/U 
             ratio_tr_pu = 1
-            line_g2_pu = n.get_lines().at[line, "g2"] / sus_pu_coeff # TO DO: to be replaced with pu getters
-            line_r = n.get_lines().at[line, "r"]
-            line_x = n.get_lines().at[line, "x"]
+            line_g2_pu = row["g2"] / sus_pu_coeff # TO DO: to be replaced with pu getters
+            line_r = row["r"]
+            line_x = row["x"]
             line_y_pu = 1 / np.sqrt(line_r * line_r + line_x * line_x) / sus_pu_coeff
             line_ksi = np.arctan2(line_r,line_x)
             lhs += 1 * m.V[i] * ( line_g2_pu * 1 * m.V[i] - line_y_pu * ratio_tr_pu * m.V[j] * pyo.sin(line_ksi + 0 - 0 + m.Phi[j] - m.Phi[i]) + line_y_pu * 1 * m.V[i] * pyo.sin(line_ksi) )
-    for tr in two_w_transf:
-        if n.get_2_windings_transformers().at[tr, "bus1_id"] == i and n.get_2_windings_transformers().at[tr, "bus2_id"] in buses:
-            j = n.get_2_windings_transformers().at[tr, "bus2_id"]
+    for id_tr, row in n.get_2_windings_transformers().iterrows():
+        if row["bus1_id"] == i and row["bus2_id"] in buses:
+            is_empy = False
+            j = row["bus2_id"]
             v_pu_coeff_j = n.get_voltage_levels().at[n.get_buses().at[j, "voltage_level_id"], "nominal_v"]
             sus_pu_coeff = p_pu / v_pu_coeff_i / v_pu_coeff_j #POWER/U/U
             ratio_tr_pu_coeff = v_pu_coeff_j / v_pu_coeff_i
-            ratio_tr_pu = n.get_2_windings_transformers().at[tr, "rated_u2"] / n.get_2_windings_transformers().at[tr, "rated_u1"] / ratio_tr_pu_coeff
-            line_r = n.get_2_windings_transformers().at[tr, "r"]
-            line_x = n.get_2_windings_transformers().at[tr, "x"]
+            ratio_tr_pu = row["rated_u2"] / row["rated_u1"] / ratio_tr_pu_coeff
+            line_r = row["r"]
+            line_x = row["x"]
             line_y_pu = 1 / np.sqrt(line_r * line_r + line_x * line_x) / sus_pu_coeff
             line_ksi = np.arctan2(line_r,line_x)
             lhs += ratio_tr_pu * m.V[i] * (0 + line_y_pu * ratio_tr_pu * m.V[i] * pyo.sin(line_ksi) - line_y_pu * 1 * m.V[j] * pyo.sin(line_ksi - 0 + 0 - m.Phi[i] + m.Phi[j]) )
-        elif n.get_2_windings_transformers().at[tr, "bus2_id"] == i and n.get_2_windings_transformers().at[tr, "bus1_id"] in buses:
-            j = n.get_2_windings_transformers().at[tr, "bus1_id"]
+        elif row["bus2_id"] == i and row["bus1_id"] in buses:
+            is_empy = False
+            j = row["bus1_id"]
             v_pu_coeff_j = n.get_voltage_levels().at[n.get_buses().at[j, "voltage_level_id"], "nominal_v"]
             sus_pu_coeff = p_pu / v_pu_coeff_i / v_pu_coeff_j #POWER/U/U 
             ratio_tr_pu_coeff = v_pu_coeff_i / v_pu_coeff_j
-            ratio_tr_pu = n.get_2_windings_transformers().at[tr, "rated_u2"] / n.get_2_windings_transformers().at[tr, "rated_u1"] / ratio_tr_pu_coeff
-            line_r = n.get_2_windings_transformers().at[tr, "r"]
-            line_x = n.get_2_windings_transformers().at[tr, "x"]
+            ratio_tr_pu = row["rated_u2"] / row["rated_u1"] / ratio_tr_pu_coeff
+            line_r = row["r"]
+            line_x = row["x"]
             line_y_pu = 1 / np.sqrt(line_r * line_r + line_x * line_x) / sus_pu_coeff
             line_ksi = np.arctan2(line_r,line_x)
             lhs += 1 * m.V[i] * (0 - line_y_pu * ratio_tr_pu * m.V[j] * pyo.sin(line_ksi + 0 - 0 + m.Phi[j] - m.Phi[i]) + line_y_pu * 1 * m.V[i] * pyo.sin(line_ksi) )
-    for gen in generators:
-        if n.get_generators().at[gen, "bus_id"] == i:
-            rhs += n.get_generators().at[gen, "target_p"] / p_pu
-    for ld in loads:
-        if n.get_loads().at[ld, "bus_id"] == i:
-            rhs -= n.get_loads().at[ld, "p0"] / p_pu
-    return lhs == rhs
+    for id_gen, row in n.get_generators().iterrows():
+        if row["bus_id"] == i:
+            rhs += row["target_p"] / p_pu
+    for id_ld, row in n.get_loads().iterrows():
+        if row["bus_id"] == i:
+            rhs -= row["p0"] / p_pu
+    if is_empty:
+        return pyo.Constraint.Skip
+    else:
+        return lhs == rhs
 
 model.ActivePowerBalance = pyo.Constraint(model.I,rule=active_power_balance_expr)
-
-
-

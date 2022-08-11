b4b11f9f2ae820552a6f60744494b864f4692bfd
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2022-08-11 22:07:05 +0200

    Read synchronous machine attrbutes example
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>

--- simulator/short-circuit-cgmes/src/main/java/com/powsybl/incubator/simulator/shortcircuit/cgmes/CgmesShortCircuitImportPostProcessor.java
+++ simulator/short-circuit-cgmes/src/main/java/com/powsybl/incubator/simulator/shortcircuit/cgmes/CgmesShortCircuitImportPostProcessor.java
@@ -8,7 +8,12 @@ package com.powsybl.incubator.simulator.shortcircuit.cgmes;
 
 import com.google.auto.service.AutoService;
 import com.powsybl.cgmes.conversion.CgmesImportPostProcessor;
+import com.powsybl.iidm.network.Generator;
 import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.extensions.GeneratorShortCircuitAdder;
+import com.powsybl.incubator.simulator.util.extensions.iidm.GeneratorShortCircuitAdder2;
+import com.powsybl.triplestore.api.PropertyBag;
+import com.powsybl.triplestore.api.QueryCatalog;
 import com.powsybl.triplestore.api.TripleStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,10 +33,28 @@ public class CgmesShortCircuitImportPostProcessor implements CgmesImportPostProc
         return NAME;
     }
 
+    private final QueryCatalog queryCatalog = new QueryCatalog("short-circuit.sparql");
+
+    private void processSynchronousMachines(Network network, TripleStore tripleStore) {
+        for (PropertyBag propertyBag : tripleStore.query(queryCatalog.get("synchronousMachineShortCircuit"))) {
+            String id = propertyBag.getId("ID");
+            double r0 = propertyBag.asDouble("r0");
+            double r2 = propertyBag.asDouble("r2");
+            double x0 = propertyBag.asDouble("r0");
+            double x2 = propertyBag.asDouble("r2");
+            Generator generator = network.getGenerator(id);
+            double directTransX = x2; // FIXME
+            generator.newExtension(GeneratorShortCircuitAdder.class)
+                    .withDirectTransX(x2)
+                    .add();
+            generator.newExtension(GeneratorShortCircuitAdder2.class)
+                    .add();
+        }
+    }
+
     @Override
     public void process(Network network, TripleStore tripleStore) {
         LOGGER.info("Loading CGMES short circuit data...");
-        // TODO
-
+        processSynchronousMachines(network, tripleStore);
     }
 }

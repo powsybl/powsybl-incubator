630c3fb63bebd8048612d96f93448038f2990b29
Christian Biasuzzi <christian.biasuzzi@soft.it>
2023-12-01 17:54:59 +0100

    restores jsx components (replacing React.createElements)
    Signed-off-by: Christian Biasuzzi <christian.biasuzzi@soft.it>

--- network-map-viewer/vite.config.js
+++ network-map-viewer/vite.config.js
@@ -5,6 +5,18 @@ const path = require('path');
 
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    loader: "jsx",
+    include: [
+      // Business as usual for .jsx
+      "src/**/*.jsx",
+      "node_modules/**/*.jsx",
+
+      // Allow .js files to contain JSX
+      "src/**/*.js",
+    ],
+    exclude: [],
+  },
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.js'),
@@ -13,7 +25,7 @@ export default defineConfig({
       fileName: 'network-map-viewer',
     },
     rollupOptions: {
-      external: ['react', 'react-dom', 'react-intl', /^@mui\/.+$/],
+      external: ['react', 'react-dom', 'react-intl', 'react/jsx-runtime', /^@mui\/.+$/],
     },
   },
 })

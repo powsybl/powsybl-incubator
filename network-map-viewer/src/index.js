ff063478dccc90ddb3a9a7bed04b6802e3b51c94
Florian Dupuy <66690739+flo-dup@users.noreply.github.com>
2024-04-15 16:12:32 +0200

    Remove network map viewer (#269)
    * Revert "Network map viewer built with Vite (#267)": This reverts commit f535583d2cda42580588c3f6bf56f7f7d25bf4dc.
    * Revert "network-map-viewer react component (extracted from gridsuite' gridstudy-app) (#263)": This reverts commit 58f6214e81af89fcdbff8db5cf0a6741ee41ea13.
    
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- network-map-viewer/src/index.js
+++
@@ -1,4 +0,0 @@
-export { GeoData } from './components/network/geo-data';
-export { LineFlowMode, LineFlowColorMode, LineLoadingZone, getLineLoadingZone } from './components/network/line-layer';
-export { MapEquipmentsBase } from './components/network/map-equipments-base';
-export { default as NetworkMap } from './components/network/network-map';
